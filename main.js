7ed9ed879942c4e4666fbc477796ea3bbf26da97
Eleni Zoumpoulaki <elenizoump@hotmail.com>
2021-12-03 23:26:08 +0100

    initial commit, skeleton of the electron app, hello world displaying with dev tools functionality.

---
+++ main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+  win.loadFile('index.html');
+  win.webContents.openDevTools();
+};
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
