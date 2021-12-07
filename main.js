a5ac8f46a7b6cbc9678aebe84ee4daa561736b16
Justin Blalock <justin.m.blalock@gmail.com>
2021-12-07 11:08:03 -0600

    implemented DarkMode

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron');
 const path = require('path');
+
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
@@ -8,13 +9,33 @@ const createWindow = () => {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
+
   win.loadFile('index.html');
   win.webContents.openDevTools();
+
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
+
 };
 
 app.whenReady().then(() => {
   createWindow();
+
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
+
+  app.on('window-all-closed', () => {
+      app.quit()
+  })
 });
