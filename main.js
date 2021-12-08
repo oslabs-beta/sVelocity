07e01fa9d919a9dfa773ce7679c7579e402c39c2
Justin Blalock <justin.m.blalock@gmail.com>
2021-12-08 10:43:54 -0600

    open file button working correctly, displaying string file contents

--- main.js
+++ main.js
@@ -1,66 +1,68 @@
-const { app, BrowserWindow, ipcMain, nativeTheme, dialog } = require('electron');
-const path = require('path');
-const { promises: fs } = require('fs');
+const {
+  app,
+  BrowserWindow,
+  ipcMain,
+  nativeTheme,
+  dialog,
+} = require("electron");
+const path = require("path");
+const { promises: fs } = require("fs");
 
 const createWindow = () => {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(app.getAppPath(), "preload.js"),
     },
   });
 
-  win.loadFile('index.html');
+  win.loadFile("index.html");
   win.webContents.openDevTools();
-  getFileFromUser();
 
-  ipcMain.handle('dark-mode:toggle', () => {
+  ipcMain.handle("dark-mode:toggle", () => {
     if (nativeTheme.shouldUseDarkColors) {
-      nativeTheme.themeSource = 'light'
+      nativeTheme.themeSource = "light";
     } else {
-      nativeTheme.themeSource = 'dark'
+      nativeTheme.themeSource = "dark";
     }
-    return nativeTheme.shouldUseDarkColors
-  })
+    return nativeTheme.shouldUseDarkColors;
+  });
 
-  ipcMain.handle('dark-mode:system', () => {
-    nativeTheme.themeSource = 'system'
-  })
-
+  ipcMain.handle("dark-mode:system", () => {
+    nativeTheme.themeSource = "system";
+  });
 };
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on('activate', () => {
+  app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
 
-  app.on('window-all-closed', () => {
-    app.quit()
-  })
+  app.on("window-all-closed", () => {
+    app.quit();
+  });
 });
 
-//codeblock which allows us to open files and read files 
-const getFileFromUser = async () => {
-   try {
-  const files = await dialog.showOpenDialog({
-    properties: ['openFile'],
-    filters: [
-      { name: 'Markdown Files', extensions: ['md', 'mdown', 'markdown'] },
-      { name: 'Svelte Files', extensions: ['.svelte'] },
-      { name: 'Markup Files', extensions: ['.html'] },
-      { name: 'Javascript Files', extensions: ['.js'] },
-      { name: 'Style Files', extensions: ['.css'] }
-    ]
-  });
-  const file = files.filePaths[0]
-  const content = await fs.readFile(file, "utf-8");
-  console.log(content);
-  }
-  catch (error) {
+//codeblock which allows us to open files and read files
+ipcMain.handle("getFileFromUser", async () => {
+  try {
+    const files = await dialog.showOpenDialog({
+      properties: ["openFile"],
+      filters: [
+        { name: "Markdown Files", extensions: ["md", "mdown", "markdown"] },
+        { name: "Svelte Files", extensions: [".svelte"] },
+        { name: "Markup Files", extensions: [".html"] },
+        { name: "Javascript Files", extensions: [".js"] },
+        { name: "Style Files", extensions: [".css"] },
+      ],
+    });
+    const file = files.filePaths[0];
+    const content = await fs.readFile(file, "utf-8");
+    console.log(content);
+  } catch (error) {
     console.log("error", error);
   }
-};
-
+});
