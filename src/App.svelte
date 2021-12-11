65786bdaee98444c03b0930412247e6dc627d14a
Justin Blalock <justin.m.blalock@gmail.com>
2021-12-11 12:16:08 -0600

    working on debugging monaco implementation
    Co-authored-by: elenizoump <elenizoump@users.noreply.github.com>
    Co-authored-by: Allie  <AllieMarzieh@users.noreply.github.com>

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,11 @@
+<!-- <script>
+  import * as monaco from "monaco-editor";
+  monaco.editor.create(document.getElementById("container"), {
+    value: 'console.log("Hello, world")',
+    language: "javascript",
+  });
+</script> -->
+
 <h1>This is the monaco component.</h1>
 
 <!-- <style>
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const webpack = require("webpack");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
+const monacoEditorWebpackPlugin = require("monaco-editor-webpack-plugin");
 const mode = process.env.NODE_ENV || "development";
 const prod = mode === "production";
 
@@ -9,7 +9,6 @@ const config = {
   mode: mode,
   entry: "./src/index.js",
   target: "electron-main",
-  // target: "electron-main",
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
@@ -19,30 +18,50 @@ const config = {
     rules: [
       {
         test: /\.svelte$/,
+        exclude: /node_modules/,
         loader: "svelte-loader",
         options: {
           preprocess: require("svelte-preprocess")({ postcss: true }),
         },
       },
+
+      // {
+      //   test: /\.worker\.js$/,
+      //   exclude: /node_modules/,
+      //   use: {
+      //     loader: 'worker-loader',
+      //     options: {
+      //       name: '[name].js',
+      //     },
+      //   },
+      // },
       {
         test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: {
-              importLoaders: 1,
-            },
-          },
-          "postcss-loader",
-        ],
+        // use: [
+        //   MiniCssExtractPlugin.loader,
+        //   {
+        //     loader: "css-loader",
+        //     options: {
+        //       importLoaders: 1,
+        //     },
+        //   },
+        //   "postcss-loader",
+        // ],
+        use:
+          process.env.NODE_ENV === "production"
+            ? [MiniCssExtractPlugin.loader, "css-loader"]
+            : ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.ttf$/,
+        use: ["file-loader"],
       },
     ],
   },
   resolve: {
     extensions: [".mjs", ".js", ".svelte"],
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [new MiniCssExtractPlugin(), new monacoEditorWebpackPlugin()],
 };
 
 module.exports = config;
