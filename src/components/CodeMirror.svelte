125d362101ba52d1d0e62cc7a5d3503e7c23cb5f
tehyaarassman <tehyaarassman@gmail.com>
2021-12-13 10:27:59 -0600

    added a codemirror editor

--- src/components/CodeMirror.svelte
+++
@@ -1,8 +0,0 @@
-<!-- <script>
- import 'codemirror/mode/javascript/javascript'
- import CodeMirror from '@joshnuss/svelte-codemirror'
-  
-  let editor
-</script>
-
-<CodeMirror bind:editor options={{ lineNumbers: true, mode: "javascript"}} value='const x = 42'/> -->
